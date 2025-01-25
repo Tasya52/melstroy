62dc81eb6cee85f0bc7d43657379d4982f7e8bfa
Tasya52 <mkunity2024@gmail.com>
2025-01-25 18:56:57 +0700

    Add files via upload

--- NewBehaviourScript.cs
+++ NewBehaviourScript.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    //Здоровье NPC
+   public int heant = 5;
+
+    //Уровень NPC
+   public int level = 1;
+
+    //Скорость NPC
+   public float speed = 1.2f;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("crickets")
+        
     }
 }
